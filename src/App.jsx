0a4a45af8c77934839ea428cfae2e5a3aff6c0d6
Nitroooz <pedrohmedeiros9@gmail.com>
2023-09-09 16:45:22 -0300

    09.09.23

--- src/App.jsx
+++ src/App.jsx
@@ -11,51 +11,60 @@ function App() {
   });
 
   const [textInput, setTextInput] = useState('');
+  const [tInput, setTInput] = useState('');
+  const [posts, setPosts] = useState([{ user: "Vitor", text: "alow", photo: "", time: 0 }]);
+  const [campoTextoVisivel, setCampoTextoVisivel] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const [tInput, setTInput] = useState("");
-
-  const [posts, setPosts] = useState([{user: "Vitor", text: "alow", photo: "",time:0}]);
-  
   useEffect(() => {
-    activateUpdate(setPosts);
+    activateUpdate((newPosts) => {
+      // Ordena os novos posts de forma decrescente
+      newPosts.sort((a, b) => {
+        let da = new Date(b.time),
+          db = new Date(a.time);
+        return da - db;
+      });
+      setPosts(newPosts);
+    });
     window.scrollTo(0, document.body.scrollHeight);
   }, []);
 
-  useEffect(()=>{
-    setPosts(posts.sort((a, b) => {
-      let da = new Date(a.time),
-          db = new Date(b.time);
-      return da - db;
-    }));
-  })
-  
-  const [campoTextoVisivel, setCampoTextoVisivel] = useState(false);
-
-  const buttonCriarPost = document.getElementById('buttonCriarPost');
-  const divEnviarMsg = document.getElementById('divEnviarMsg');
-
-  const handleClick = () => {
-    // Alterna a exibição do campo de texto (de oculto para visível e vice-versa)
-    if (!campoTextoVisivel) {
-      divEnviarMsg.style.display = 'block';
-      buttonCriarPost.style.display = 'none';
+  useEffect(() => {
+    if (user.email && user.email !== 'Email') {
+      setIsLoggedIn(true);
     } else {
-      divEnviarMsg.style.display = 'none';
-      buttonCriarPost.style.display = 'block';
+      setIsLoggedIn(false);
     }
+  }, [user]);
+
+  const handleCriarPostClick = () => {
     setCampoTextoVisivel(!campoTextoVisivel);
+  };
+
+  const handleEnviarClick = () => {
+    if (!isLoggedIn) {
+      alert("Você precisa fazer login para enviar mensagens.");
+      return;
+    }
+    
+    writeUserData(tInput);
+    setTInput('');
+    setCampoTextoVisivel(false);
   };
 
   return (
     <>
       <div className='divHeader'>
+        <h1 className='tLiveBlog'>Live Blog</h1>
         <div className='divUsuario'>
           <img src={user.photo} className="logo" alt="React logo" />
           <div className='divUserEmail'>
             <h1 className='tUserName' >{user.nome}</h1>
             <h2 className='tEmail'>{user.email}</h2>
           </div>
-          <button className='buttonCriarPost' id="buttonCriarPost" onClick={handleClick}>Criar Post</button>
+          <button className='buttonCriarPost' onClick={handleCriarPostClick}>
+            Criar Post
+          </button>
         </div>
         <div className="card">
           <button className="bLoginGoogle" onClick={() => myloginWithGoogle(setUser)}>
@@ -68,37 +77,41 @@ function App() {
       </div>
       <div className='divPosts'>
         {posts.map((post) => (
-            <div className="posts" key={post.key}>
-                <img className='photo' src={post.photo} />
-              <div className='divConteudoPosts'>
-                <h2 className='tNameUser'>{post.user}:</h2>
-                {post.time&&<h5 className='tHorario'>{`${(new Date(post.time)).getDate() < 10? "0"+(new Date(post.time)).getDate().toString() : (new Date(post.time)).getDate()}/${((new Date(post.time)).getMonth() + 1) < 10 ? "0" + ((new Date(post.time)).getMonth() + 1).toString() : ((new Date(post.time)).getMonth() + 1)}`} {`${(new Date(post.time)).getHours() < 10? "0"+(new Date(post.time)).getHours().toString() : (new Date(post.time)).getHours()}:${(new Date(post.time)).getMinutes() < 10? "0"+(new Date(post.time)).getMinutes().toString() : (new Date(post.time)).getMinutes()}`}</h5>}
-                <h3 className='tTextPost'>{post.text}</h3>
-              </div>
+          <div className="posts" key={post.key}>
+            <img className='photo' src={post.photo} alt={post.user} />
+            <div className='divConteudoPosts'>
+              <h2 className='tNameUser'>{post.user}:</h2>
+              {post.time&&<h5 className='tHorario'>{`${(new Date(post.time)).getDate() < 10? "0"+(new Date(post.time)).getDate().toString() : (new Date(post.time)).getDate()}/${((new Date(post.time)).getMonth() + 1) < 10 ? "0" + ((new Date(post.time)).getMonth() + 1).toString() : ((new Date(post.time)).getMonth() + 1)}`} {`${(new Date(post.time)).getHours() < 10? "0"+(new Date(post.time)).getHours().toString() : (new Date(post.time)).getHours()}:${(new Date(post.time)).getMinutes() < 10? "0"+(new Date(post.time)).getMinutes().toString() : (new Date(post.time)).getMinutes()}`}</h5>}
+              <h3 className='tTextPost'>{post.text}</h3>
             </div>
-          ))}
+          </div>
+        ))}
       </div>
-      <div className='divEnviarMsg' id="divEnviarMsg" style={{ display: campoTextoVisivel ? 'block' : 'none' }}>
-        <input className='input1' id='input1'
-        placeholder="Digite sua mensagem..."
-          onChange={(event) => {setTInput(event.target.value)}}
-          onKeyUp={(event)=>{ 
-              if(event.key === "Enter") {
-              writeUserData(tInput)
-              event.target.value=""
-              }}}
+      <div
+        className='divEnviarMsg'
+        style={{ display: campoTextoVisivel ? 'block' : 'none' }}
+      >
+        <input
+          className='input1'
+          id='input1'
+          placeholder="Digite sua mensagem..."
+          value={tInput}
+          onChange={(event) => setTInput(event.target.value)}
+          onKeyUp={(event) => {
+            if (event.key === "Enter") {
+              handleEnviarClick();
+            }
+          }}
         />
-        <button className="bEnviarMsg" id='bEnviarMsg' onClick={() =>{
-          writeUserData(tInput);
-          let input1 = document.getElementById("input1");
-          input1.value = "";
-        }}>
+        <button
+          className="bEnviarMsg"
+          onClick={handleEnviarClick}
+        >
           Enviar
         </button>
       </div>
     </>
   );
 }
-
 
 export default App;
